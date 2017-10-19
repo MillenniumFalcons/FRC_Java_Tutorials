edf22458a93183f65a05ceed0a6a3f2d5a9ae95f
Surya Madhan <suryamadhan@gmail.com>
2017-10-19 15:20:49 -0700

    remove the answer lol

--- Lesson_03/Exercises/ExerciseTwo.java
+++ Lesson_03/Exercises/ExerciseTwo.java
@@ -18,20 +18,5 @@ public class ExerciseTwo
 	public static void main(String[]args)
 	{
 
-		for(int i=1; i<=4; i++)
-		{
-			for(int a=3; a>3-i; a--)
-			{
-				System.out.print(" ");
-			}
-			System.out.printf("\\");
-			for(int b=1; b<10-(2*i); b++)
-			{
-				System.out.print(":");
-			}
-			System.out.print("/");
-			System.out.println();
-		}
-
 	}
 }

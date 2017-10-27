2b7c535d5e4f30adb298b6844ff5f6764abd9030
Surya <suryamadhan@gmail.com>
2017-10-27 15:50:01 -0700

    hi

---
+++ Lesson_03/Lessons/eamonTeach.java
@@ -0,0 +1,16 @@
+public class eamonTeach 
+{
+	public static void main (String[] args) 
+	{
+		for(int i=1; i<=4; i++) 
+		{
+			for(int j=1; j<=4; j++)
+			{
+				System.out.print("*");
+				
+			}
+			System.out.println();
+		}
+	}
+
+}

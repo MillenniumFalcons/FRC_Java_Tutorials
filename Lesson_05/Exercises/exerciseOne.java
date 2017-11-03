ff2b7e9d1fc5f9f91568bbc031ad9872eb92b68c
Surya <suryamadhan@gmail.com>
2017-11-03 14:34:43 -0700

    exercise one upload lesson 5

---
+++ Lesson_05/Exercises/exerciseOne.java
@@ -0,0 +1,25 @@
+public class exerciseOne
+{
+	public static void main(String[]args)
+	{
+		int variable = 1;
+		double x1 = 1;
+		double y1 = 1;
+		double x2 = 5;
+		double y2 = 1;
+		/*
+		if variable is equal to 1 then calculate the distance between 2 points
+		if variable  is equal to 2 then calculate the slope between 2 points
+		*/
+	}
+	
+	static void calcDist()//make sure you enter parameters
+	{
+		
+	}
+	
+	static void calcSlope())//make sure you enter parameters
+	{
+		
+	}
+}

2ad240f49b51ff7d0ee4099b6ae53a2a110f6362
Surya <suryamadhan@gmail.com>
2017-11-06 11:22:31 -0800

    exercise 5

--- Lesson_05/VoidFunctions/Exercises/exercise.java
+++ Lesson_05/VoidFunctions/Exercises/exercise.java
@@ -1,4 +1,19 @@
 public class exercise
 {
+	static double xComponent, yComponent, magnitude, direction;
 	
+	public static void main(String[]args)
+	{
+		
+	}
+	
+	public static void calcTheta()
+	{
+		
+	}
+	
+	public static void calcMagnitude()
+	{
+		
+	}
 }

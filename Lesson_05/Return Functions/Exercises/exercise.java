bf29225b6f030f83bdc6a53688c0c4ad4377fe1e
Surya <suryamadhan@gmail.com>
2017-11-06 12:11:41 -0800

    zip and exercise descriptions upload (lesson 5)

--- Lesson_05/Return Functions/Exercises/exercise.java
+++ Lesson_05/Return Functions/Exercises/exercise.java
@@ -1,17 +1,28 @@
-//In this exercise we will calculate the slope between 2 points and the distance between 2 points
+/*
+Lesson _05
+In this exercise, you will use return functions to calculate the slope between 2 points and the distance between 2 points
+
+If the variable var is equal to 1 then calculate the distance between 2 points
+Else If variable var is equal to 2 then calculate the slope between 2 points
+Else var does not have an appropriate value.
+
+Good Luck Completing the exercise!
+*/
+
+/*
+ * @author Surya
+ */
+
 public class exercise
 {
 	public static void main(String[]args)
 	{
-		int variable = 1;
+		int var = 1;
 		double x1 = 1;
 		double y1 = 1;
 		double x2 = 5;
 		double y2 = 1;
-		/*
-		if variable is equal to 1 then calculate the distance between 2 points
-		if variable  is equal to 2 then calculate the slope between 2 points
-		*/
+		
 	}
 	
 	static void calcDist()//make sure you enter parameters
@@ -19,7 +30,7 @@ public class exercise
 		
 	}
 	
-	static void calcSlope())//make sure you enter parameters
+	static void calcSlope()//make sure you enter parameters
 	{
 		
 	}
--- Lesson_05/VoidFunctions/Exercises/exercise.java
+++ Lesson_05/VoidFunctions/Exercises/exercise.java
@@ -1,18 +1,34 @@
+/*
+Lesson _05
+In this exercise, you will use void functions to calculate magnitude and the direction of a vector from their x-component, and y-component
+
+Feel free to look up the formulas for both finding the direction and the magnitude of a vector
+Remember that you don't need parameters for this exercise, because the variables are static
+
+Good Luck Completing the exercise!
+*/
+
+/*
+ * @author Surya
+ */
+ 
 public class exercise
 {
 	static double xComponent, yComponent, magnitude, direction;
 	
 	public static void main(String[]args)
 	{
+		xComponent = 6;
+		yComponent = 8;
 		
 	}
 	
-	public static void calcTheta()
+	public static void calcTheta() //use this function to calculate the direction of the vector
 	{
 		
 	}
 	
-	public static void calcMagnitude()
+	public static void calcMagnitude() //use this function to calculate the magnitude of the vector
 	{
 		
 	}
